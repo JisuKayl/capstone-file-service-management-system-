c1c7d4959ad62fd5a66ca1b455924274dec686a0
TheDarkrai25 <krappa123@yahoo.com>
2024-02-29 01:24:32 +0800

    adds

--- src/pages/Admin Page/AdminSpecificPage.jsx
+++ src/pages/Admin Page/AdminSpecificPage.jsx
@@ -1,8 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import AdminPageCSS from "../Admin Page/Adminpage.module.css"
 
 const AdminSpecificPage = () => {
-  return <div>This is the admin-specific page</div>;
+  return <div className={AdminPageCSS.h1}>This is the admin-specific page</div>;
+
+  
 };
 
 export default AdminSpecificPage;
--- src/pages/Student Page/StudentSpecificPage.jsx
+++ src/pages/Student Page/StudentSpecificPage.jsx
@@ -1,8 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import StudentPageCSS from "../Student Page/StudentPageCss.module.css"
 
 const StudentSpecificPage = () => {
-  return <div>This is the student-specific page</div>;
+  return <div className={StudentPageCSS.h1}>This is the student-specific page</div>;
 };
 
 export default StudentSpecificPage;
